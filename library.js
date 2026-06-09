fd51ab8a82c4db1ca2c91f82b5bfe955c493bbbc
jc-2502 <16088146+jc-2502@users.noreply.github.com>
2026-06-09 00:54:25 -0400

    Add addBookToLibrary function
    Create library array.
    addBookToLibrary creates a Book object and adds it to the library array.

--- library.js
+++ library.js
@@ -1,3 +1,5 @@
+let library = [];
+
 function Book(title, author, read) {
   if (!new.target) {
     throw Error('include new when calling constructor');
@@ -6,4 +8,10 @@ function Book(title, author, read) {
   this.title = title;
   this.author = author;
   this.read = read;
+}
+
+function addBookToLibrary(title, author, read) {
+  let book = new Book(title, author, read);
+
+  library.push(book);
 }
