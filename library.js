063062c74de9ff4696820973b636c2b0cde55bbc
jc-2502 <16088146+jc-2502@users.noreply.github.com>
2026-06-09 01:26:50 -0400

    Add unique id generation

--- library.js
+++ library.js
@@ -1,17 +1,19 @@
 let library = [];
 
-function Book(title, author, read) {
+function Book(id, title, author, read) {
   if (!new.target) {
     throw Error('include new when calling constructor');
   }
 
+  this.id = id;
   this.title = title;
   this.author = author;
   this.read = read;
 }
 
 function addBookToLibrary(title, author, read) {
-  let book = new Book(title, author, read);
+  let id = crypto.randomUUID();
+  let book = new Book(id, title, author, read);
 
   library.push(book);
 }
