f08b1f13327d629fb24cac6e484cebe21fdebfae
jc-2502 <16088146+jc-2502@users.noreply.github.com>
2026-06-11 18:32:46 -0400

    Add function to display books on page
    Add displayBooks function and call it.
    Function loops through each Book object, puts the property values of
    the book inside a container and adds that container in the books
    container.
    
    Make each book container a grid.
    Set widths to pcts so that they will be same size across rows while
    taking width of books container.

--- library.js
+++ library.js
@@ -1,5 +1,7 @@
 let library = [];
 
+const booksContainer = document.querySelector('.books');
+
 function Book(id, title, author, read) {
   if (!new.target) {
     throw Error('include new when calling constructor');
@@ -18,6 +20,30 @@ function addBookToLibrary(title, author, read) {
   library.push(book);
 }
 
+function displayBooks() {
+  for (const book of library) {
+    const bookContainer = document.createElement('div');
+    const title = document.createElement('p');
+    const author = document.createElement('p');
+    const readStatus = document.createElement('p');
+
+    title.textContent = book.title;
+    author.textContent = book.author;
+    readStatus.textContent = (book.read) ? 'read': 'not read';
+
+    bookContainer.classList.add('book');
+    title.classList.add('title');
+    author.classList.add('author');
+    readStatus.classList.add('read');
+
+    bookContainer.appendChild(title);
+    bookContainer.appendChild(author);
+    bookContainer.appendChild(readStatus);
+    booksContainer.appendChild(bookContainer);
+  };
+}
+
 addBookToLibrary('Thinking, Fast and Slow', 'Daniel Kahneman', true);
 addBookToLibrary('Predictably Irrational', 'Dan Ariely', true);
 addBookToLibrary('Nudge', 'Richard Thaler and Cass Sunstein', false);
+displayBooks();
